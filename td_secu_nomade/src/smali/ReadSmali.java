e63c937af6562f5553d36809c466c248d4ff1a8c
Elo <elodie.bouyer@hotmail.fr>
2014-11-03 11:32:53 +0100

    Fonction qui lit un fichier smali.

--- td_secu_nomade/src/smali/ReadSmali.java
+++ td_secu_nomade/src/smali/ReadSmali.java
@@ -1,15 +1,45 @@
 package smali;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ReadSmali {
-	
+
 	private String text;
-	
+
 	public ReadSmali(String path) {
 		super();
-		
+		readFile(path);
 	}
-	
+
+	/**
+	 * Donne le contenue du fichier smali.
+	 * @return
+	 */
 	public String getText() {
 		return text;
+	}
+
+	private void readFile(String path) {
+
+		try{
+			BufferedReader buff = new BufferedReader(new FileReader(path));
+
+			try {
+				String line;
+
+				while ((line = buff.readLine()) != null) {
+					text += line;
+				}
+			} 
+			finally {
+				buff.close();
+			}
+		} 
+		catch (IOException ioe) {
+
+			System.out.println("Erreur --" + ioe.toString());
+		}
 	}
 }
