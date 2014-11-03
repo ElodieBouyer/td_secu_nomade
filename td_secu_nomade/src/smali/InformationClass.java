e11a3b5c8b45d9418cf96a5fae21cf41863e177a
Elo <elodie.bouyer@hotmail.fr>
2014-11-03 11:25:18 +0100

    Ajout de la classe pour lire le smali?.

---
+++ td_secu_nomade/src/smali/InformationClass.java
@@ -0,0 +1,22 @@
+package smali;
+
+public class InformationClass {
+
+	private String name; // Nom de la classe.
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
---
+++ td_secu_nomade/src/smali/ReadSmali.java
@@ -0,0 +1,15 @@
+package smali;
+
+public class ReadSmali {
+	
+	private String text;
+	
+	public ReadSmali(String path) {
+		super();
+		
+	}
+	
+	public String getText() {
+		return text;
+	}
+}
