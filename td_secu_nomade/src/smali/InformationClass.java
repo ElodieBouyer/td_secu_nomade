9c19c8769758beeb630d6d1d9c42922a54b952de
Elo <elodie.bouyer@hotmail.fr>
2014-11-03 16:37:08 +0100

    Affichage des informations sur une classe et ses méthodes.

--- td_secu_nomade/src/smali/InformationClass.java
+++ td_secu_nomade/src/smali/InformationClass.java
@@ -1,21 +1,38 @@
 package smali;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InformationClass {
 
 	private String name; // Nom de la classe.
+	private List<InformationMethod> listMethod = new ArrayList<InformationMethod>(); // Liste des méthodes de la classe.
 
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
-
-	/**
-	 * @param name the name to set
-	 */
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public void addMethod(String name, String access) {		
+		listMethod.add(new InformationMethod(name, access));
+	}
+	
+	public String getInfo() {
+		String info = "Nom de la classe = ";
+		info += this.name;
+		info += "\n";
+		
+		int nb = 1;
+		for(InformationMethod m : listMethod) {
+			info += "  Méthode " + nb + " : ";
+			info += m.getName();
+			info += "\n";
+			info += "    Access : ";
+			info += m.getAccess();
+			nb++;
+			info += "\n \n";
+		}
+		
+		return info;
 	}
 	
 	
--- td_secu_nomade/src/smali/SmaliAnalyse.java
+++ td_secu_nomade/src/smali/SmaliAnalyse.java
@@ -1,22 +1,55 @@
 package smali;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 
 public class SmaliAnalyse {
-	
-	private InformationClass infoClass;
 
-	
+	private InformationClass infoClass;
+	private BufferedReader buffer;
+
+	static private String name = ".class";
+	static private String method = ".method";
+
 	public SmaliAnalyse() {
+		super();
 		this.infoClass = new InformationClass();
-		ReadSmali read = new ReadSmali("/home/elodie/Documents/master-2/securite_nomade/td2/Challenge5/smali/secu/fifth/AccessVerifier.smali");
+	}
+
+	public String getInfo(BufferedReader buffer) {
+		this.buffer = buffer;
+
+		findInfo();
+
+		return infoClass.getInfo();
+	}
+
+	private void findInfo() {
+		String line = "";
 		try {
-			System.out.println(read.getBuffer().readLine());
+			while( (line = buffer.readLine()) != null) {
+				if( line.contains(name) ) {
+					while( line.contains(" ") ) {
+						line = line.substring(line.indexOf(" ")+1);
+					}
+					this.infoClass.setName(line.substring(0, line.length()-1));
+				}
+				
+				else if( line.contains(method)) {
+					line = line.substring(method.length()+1);
+					String access = line.substring(0,line.indexOf(' '));
+
+					line = line.substring(access.length()+1);
+					String name;
+					if( line.indexOf(' ') == -1 ) name = line.substring(0,line.indexOf('('));
+					else name = line.substring(0,line.indexOf(' '));
+					
+					this.infoClass.addMethod(name, access);
+				}
+			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 }
