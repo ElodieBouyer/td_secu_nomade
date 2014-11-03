a18c08e9e145137731aa046a015fb2ff09619d89
Fizari <fauveauvincent@gmail.com>
2014-11-03 13:09:12 +0100

    Avancement de GenerationDot

--- td_secu_nomade/src/smali/GenerationDot.java
+++ td_secu_nomade/src/smali/GenerationDot.java
@@ -3,6 +3,7 @@ package smali;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.StringTokenizer;
 
 public class GenerationDot {
 
@@ -12,7 +13,20 @@ public class GenerationDot {
 		this.calls = new HashMap<String,Set<String>>();
 	}
 	
-	public String getNextFunction(){
+	public String getNextFunction(String smali){
+		StringTokenizer st = new StringTokenizer(smali);
+		String mot = "";
+		while (st.hasMoreElements()){
+			mot = st.nextToken();
+			if (mot.equals("invoke-super") || mot.equals("invoke-virtual") || mot.equals("invoke-direct") 
+					|| mot.equals("invoke-static")|| mot.equals("invoke-interface")|| mot.equals("invoke-virtual/range")
+					|| mot.equals("invoke-direct/range")|| mot.equals("invoke-super/range")
+					|| mot.equals("invoke-static/range")|| mot.equals("invoke-interface-range")){
+				
+				StringTokenizer st2 = new StringTokenizer(mot,"->");
+				
+			}
+		}
 		return "lol";
 	}
 }
